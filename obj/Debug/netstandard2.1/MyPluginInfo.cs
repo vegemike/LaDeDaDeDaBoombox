bef1878b98aa64625302520b90066ccc10bbb1b4
Michael Stradling <116464261+vegemike@users.noreply.github.com>
2025-06-02 18:51:48 +0100

    Delete obj/Debug/netstandard2.1 directory

--- obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace LaDeDaDeDaBoombox
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "vegemike.LaDeDaDeDaBoombox";
-        public const string PLUGIN_NAME = "LaDeDaDeDaBoombox";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
